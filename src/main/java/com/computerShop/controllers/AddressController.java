a6a17e86ae1eb2b18707b4e92228458857e68032
Adrian <adek9025@gmail.com>
2021-01-15 09:26:58 +0100

    commit

--- src/main/java/com/computerShop/controllers/AddressController.java
+++ src/main/java/com/computerShop/controllers/AddressController.java
@@ -5,10 +5,9 @@ import com.computerShop.Utils.View;
 import com.computerShop.services.AddressService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -18,9 +17,9 @@ public class AddressController {
     @Autowired
     private AddressService addressService;
 
-    @PostMapping("/address")
-    @ResponseBody
-    public void addAddress(@RequestBody Address address, Principal principal){
+    @PostMapping(value = "/address", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addAddress(@ModelAttribute Address address, Principal principal){
         addressService.addAddress(address, principal);
     }
 
